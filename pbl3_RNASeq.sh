be77a2f6b953b2a948ad1f9cb77b57d5b8d78aec
zchah001 <72586266+zchah001@users.noreply.github.com>
2020-12-05 21:43:26 -0800

    Update pbl3_RNASeq.sh

--- pbl3_RNASeq.sh
+++ pbl3_RNASeq.sh
@@ -11,18 +11,19 @@ DB=M_tuberculosis.cds.fasta
 
 # INDEX THE DB FOR KALLISTO
 INDEX=$(basename $DB .fasta).idx
+
 # YOUR CODE HERE TO CREATE THIS INDEX
+kallisto index -i $INDEX $DB
 
 ## END INDEX
 
 mkdir -p results
-
-
 tail -n +2 M_tuberculosis/sra_info.tab | while read RUN CARBON SAMPLE REP
 do
-	OUTNAME=results/${CARBON}_pH${SAMPLE}_r$REP
-	FASTQFILE=M_tuberculosis/$RUN.fastq.gz
-	echo "RUN is $RUN $OUTNAME INFILE=$FASTQFILE"
-	#RUN KALLISTO ON THE FILE
-#	kallisto quant -i $INDEX --single -l 300 -s 30 [ADD SOMETHING HERE]
+OUTNAME=results/${CARBON}_pH${SAMPLE}_r$REP
+FASTQFILE=M_tuberculosis/$RUN.fastq.gz
+echo "RUN is $RUN $OUTNAME INFILE=$FASTQFILE"
+#RUN KALLISTO ON THE FILE
+kallisto quant -i $INDEX --single -l 300 -s 30 $FASTQFILE -o $OUTNAME
 done
+
