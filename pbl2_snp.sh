930b2508c12461b71b869c1a17c2d3a1ac77d1b2
zchah001 <72586266+zchah001@users.noreply.github.com>
2020-12-06 14:54:14 -0800

    Update pbl2_snp.sh

--- pbl2_snp.sh
+++ pbl2_snp.sh
@@ -1,4 +1,3 @@
-
 #!/usr/bin/bash
 #SBATCH -p short -N 1 -n 8 --out snp.log
 
@@ -9,25 +8,17 @@ CPU=8
 
 FOLDER=/bigdata/gen220/shared/data/SARS-CoV-2/
 GENOME=NC_045512.fa
-if [ ! -L $GENOME ]; then
-        # make a symlink
-        ln -s $FOLDER/$GENOME .
-fi
-
-if [ ! -f $GENOME.pac ]; then
-        bwa index $GENOME
-
-GENOME=NC_045512.fa
 bwa index $GENOME
 
-for acc in $(cat acc.txt)
+for acc  in $(cat acc.txt)
 do
-  m="$a.bam $m"
-
+  	m="${acc}.bam $m"
+        
 VCF=SARS-CoV-2.vcf.gz
 bcftools mpileup -Ou -f $GENOME $m | bcftools call --ploidy 1 -vmO z -o $VCF
 tabix -p vcf $VCF
 bcftools stats -F $GENOME -s - $VCF > $VCF.stats
 mkdir -p plots
 plot-vcfstats -p plots/ $VCF.stats
-done 
+
+done
